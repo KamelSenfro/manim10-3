0b52ff8609a5ff1231d7401331f0c8043f471dce
KamelSenfro <kamel.fares02@eng-st.cu.edu.eg>
2024-03-19 07:38:27 +0200

    sas

--- LosCircle.py
+++ LosCircle.py
@@ -1,18 +1,36 @@
-#LosCircle
 from manim import *
 
-class CodeFromString(Scene):
+class CircleDivision(Scene):
     def construct(self):
-        code = '''from manim import Scene, Square
+        # Create a circle
+        circle = Circle(radius=2, color=BLUE, fill_color=BLUE, fill_opacity=0.3)
 
-class FadeInSquare(Scene):
+        # Create a line to divide the circle into two halves
+        dividing_line1 = Line(start=circle.point_from_proportion(0), end=circle.point_from_proportion(0.5), color=RED)
+        dividing_line2 = Line(start=circle.point_from_proportion(0.75), end=circle.point_from_proportion(0.25), color=RED)
+
+        self.play(Create(circle))
+        self.wait(1)
+        self.play(Create(dividing_line1))
+        self.play(Create(dividing_line2))
+        self.wait(1)
+
+      #manim -pql LosCircle.py CircleDivision
+        
+    
+class PointsOnCircle(Scene):
     def construct(self):
-        s = Square()
-        self.play(FadeIn(s))
-        self.play(s.animate.scale(2))
-        self.wait()
-'''
-        rendered_code = Code(code=code, tab_width=4, background="window",
-                            language="Python", font="Monospace")
-        self.add(rendered_code)
-        self.play(rendered_code.code())
+        circle = Circle(radius=3.0, color=GREEN)
+        # Number of points required
+        num_points = 16
+        # Calculate each angle
+        angles = [n * (360 / num_points) for n in range(num_points)]
+        # Points on circumference of circle
+        points = [circle.point_at_angle(n*DEGREES) for n in angles]
+        # Create circles at each point
+        circles = [Circle(radius=0.05, color=RED, fill_opacity=1).move_to(p) for p in points]
+        # Add the circle to the scene
+        self.add(circle)
+        # Add each of the points to the scene
+        for c in circles:
+            self.add(c)    
